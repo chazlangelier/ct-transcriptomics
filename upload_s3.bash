eaee091ae3eb35b71a3f8637c2e4a8dc9eca58d6
Jack Kamm <jackkamm@gmail.com>
2018-12-31 00:23:37 +0000

    Prevent re-uploading files to S3 unnecessarily

--- upload_s3.bash
+++ upload_s3.bash
@@ -1,12 +1,9 @@
 #!/bin/bash
-set -e
-set -x
+set -ex
 
-# TODO can't comment out lines, find a better way to avoid re-uploading here
-for fname in \
-    GCF_000068585.1_ASM6858v1_genomic.fna GCF_000068585.1_ASM6858v1_genomic.gff \
-    OPS016_CT_Transcriptome_RNA_8hpi-1a_I9_S191.bam \
-    OPS016_CT_Transcriptome_RNA_8hpi-1a_I9_S191.featureCounts \
-    OPS016_CT_Transcriptome_RNA_8hpi-1a_I9_S191.featureCounts.summary; do
-    aws s3 cp scratch/$fname s3://jackkamm/ct-transcriptomics/$fname
-done
+aws s3 sync scratch/ s3://jackkamm/ct-transcriptomics/ --exclude '*' \
+    --include GCF_000068585.1_ASM6858v1_genomic.fna \
+    --include GCF_000068585.1_ASM6858v1_genomic.gff \
+    --include OPS016_CT_Transcriptome_RNA_8hpi-1a_I9_S191.bam \
+    --include OPS016_CT_Transcriptome_RNA_8hpi-1a_I9_S191.featureCounts \
+    --include OPS016_CT_Transcriptome_RNA_8hpi-1a_I9_S191.featureCounts.summary
